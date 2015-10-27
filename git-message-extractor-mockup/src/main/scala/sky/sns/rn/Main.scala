a0d9f36c4f775e17ae7ff2d462b59767869af83b
laksithar <lakshitha.hav@gmail.com>
2015-10-27 00:00:42 +0000

    [Sam] reading initial commit from http client

--- git-message-extractor-mockup/src/main/scala/sky/sns/rn/Main.scala
+++ git-message-extractor-mockup/src/main/scala/sky/sns/rn/Main.scala
@@ -6,8 +6,8 @@ import scalaj.http.HttpRequest
 
 object Main {
   def main(args: Array[String]) {
-    val request: HttpRequest = HttpConnections.get("https://api.github.com/repos/lranasingha/release_notes/git/commits/")
+    val request: HttpRequest = HttpConnections.get("https://api.github.com/repos/lranasingha/release_notes/git/commits/119684f145868fe0b08410115d4fa6aef234c8ac")
     val response = SyncHttpClient().makeRequest(request)
-    println("git commit GET respone " + response)
+    println("git commit -> " + response.body)
   }
 }
