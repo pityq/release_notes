4937ee0ff813f1a6e72e0c746757e035e756af0c
laksithar <lakshitha.hav@gmail.com>
2015-11-15 23:43:39 +0000

    [Sam] unpacking json nodes to build commits

--- git-message-extractor-mockup/src/main/scala/sky/sns/rn/Main.scala
+++ git-message-extractor-mockup/src/main/scala/sky/sns/rn/Main.scala
@@ -1,35 +1,25 @@
 package sky.sns.rn
 
-import org.json4s.jackson.Json
-import org.json4s.{DefaultFormats, StreamInput}
 import sky.sns.rn.data.Commit
 import sky.sns.rn.http.{AsyncHttpClient, HttpConnections, SyncHttpClient}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
-import scalaj.http.{HttpRequest, HttpResponse}
+import scalaj.http.HttpRequest
 
 object Main {
   def main(args: Array[String]) {
     val request: HttpRequest = HttpConnections.get("https://api.github.com/repos/lranasingha/release_notes/git/commits/119684f145868fe0b08410115d4fa6aef234c8ac")
     val syncResponse = SyncHttpClient().makeRequest(request)
 
-    println("git commits -> " + syncResponse.body)
-    Future {
-      request.execute(stream => {
-        val node = Json(DefaultFormats).parse(new StreamInput(stream))
-        node
-      })
-    } onComplete {
-      case Success(res) => println("success response " + res.body)
+    val asyncResponse: Future[Seq[Commit]] = AsyncHttpClient().makeRequest(HttpConnections.get("https://api.github.com/repos/lranasingha/release_notes/commits"))
+    asyncResponse onComplete {
+      case Success(res) => println("success response " + res)
       case Failure(ex) => println("error " + ex.getMessage)
     }
 
-    val asyncResponse: Future[HttpResponse[Seq[Commit]]] = AsyncHttpClient().makeRequest(HttpConnections.get("https://api.github.com/repos/lranasingha/release_notes/git/commits/"))
-    asyncResponse onComplete {
-      case Success(res) => println("success response " + res.body)
-      case Failure(ex) => println("error " + ex.getMessage)
-    }
+    Await.result(asyncResponse, 5 seconds)
   }
 }
--- git-message-extractor-mockup/src/main/scala/sky/sns/rn/http/AsyncHttpClient.scala
+++ git-message-extractor-mockup/src/main/scala/sky/sns/rn/http/AsyncHttpClient.scala
@@ -1,29 +1,40 @@
 package sky.sns.rn.http
 
+import org.json4s.JsonAST.{JObject, JValue}
 import org.json4s.jackson.Json
-import org.json4s.{DefaultFormats, JValue, StreamInput}
+import org.json4s.{DefaultFormats, StreamInput}
 import sky.sns.rn.AsyncServiceClient
-import sky.sns.rn.data.{Commit, Serializers}
+import sky.sns.rn.data.{Author, Commit, Committer, Serializers}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scalaj.http.{HttpRequest, HttpResponse}
+import scalaj.http.HttpRequest
 
 object AsyncHttpClient {
   def apply(): AsyncHttpClient = new AsyncHttpClient()
 }
 
-class AsyncHttpClient extends AsyncServiceClient[HttpRequest, HttpResponse[Seq[Commit]]] {
+class AsyncHttpClient extends AsyncServiceClient[HttpRequest, Seq[Commit]] {
   private implicit val formats = DefaultFormats + Serializers.commitSerializer()
   private val jsonParser = Json(DefaultFormats)
 
-  override def makeRequest(request: HttpRequest): Future[HttpResponse[Seq[Commit]]] = {
-    Future[HttpResponse[Seq[Commit]]] {
-      request.execute(stream => {
-        val nodes = jsonParser.parse(StreamInput(stream))
-        Seq.empty[Commit]
-      })
-    }
+  override def makeRequest(request: HttpRequest) = Future[Seq[Commit]] {
+    request.execute(stream => {
+      val nodes = jsonParser.parse(StreamInput(stream))
+      nodes.children.map(n => extractFields(n))
+    }).body
   }
 
+  def extractFields(node: JValue): Commit = node match {
+    case jObj: JObject => {
+      println(jObj.values("sha"))
+      jObj.values("commit") match {
+        case commit: Map[String, Any] => println(s"commit " + commit("author"))
+      }
+      println(jObj.values("committer"))
+      println(jObj.values("url"))
+      println()
+    }
+      Commit("sha", "message", Author("name", "email", 1), Committer("name", "email", 2))
+  }
 }
